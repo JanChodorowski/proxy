f3b8b3ade7da6f84cab5ca288a6650e8a398bb26
JanChodorowski <chodorowski.j@gmail.com>
2022-05-16 11:40:08 +0200

    Create proxy:

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		link := r.URL.Query().Get("link")
+
+		if link == "" {
+			http.Error(w, "no link to asset provided", http.StatusBadRequest)
+		}
+
+		resp, err := http.Get(link)
+
+		if err != nil {
+			fmt.Fprint(w, "Error")
+		}
+
+		bytes, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(bytes)
+
+	})
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		panic("Port not specified")
+	}
+
+	log.Fatal(http.ListenAndServe(":"+port, nil))
+
+}
